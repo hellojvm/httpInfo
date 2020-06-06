d5e776e2fc1e05a744f6a5313f8ff1d57fc587c8
fudong <fudong@wowkai.cn>
2020-06-06 23:32:46 +0800

    log

--- src/main/java/com/fd/http/web/HomeController.java
+++ src/main/java/com/fd/http/web/HomeController.java
@@ -6,6 +6,8 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,8 +16,11 @@ import com.fd.http.web.vo.HttpInfo;
 @RestController
 @RequestMapping("/home")
 public class HomeController {
+	private static final Logger log = LoggerFactory.getLogger(HomeController.class);
+
 	@RequestMapping
 	public HttpInfo index(HttpServletRequest req) {
+		log.info("开始:{}", System.currentTimeMillis());
 		Map<String, String> heads = new HashMap<String, String>();
 		Enumeration<String> ns = req.getHeaderNames();
 		while (ns.hasMoreElements()) {
@@ -23,6 +28,7 @@ public class HomeController {
 			heads.put(name, req.getHeader(name));
 		}
 		HttpInfo hi = new HttpInfo(req.getCookies(), req.getParameterMap(), heads, req.getRemoteAddr());
+		log.info("结束：{}", System.currentTimeMillis());
 		return hi;
 	}
 }
